6b8012ffb3213403f34381deabd30af1ecd2ebb4
daniperezdpj <82525461+daniperezdpj@users.noreply.github.com>
2021-11-12 17:57:30 -0400

    Add files via upload

---
+++ BottomNavbar.js
@@ -0,0 +1,37 @@
+import {  Nav,Navbar} from 'react-bootstrap';
+import  '../index.css';
+import { Styledbottomnavbar, } from './styles'
+import {Link} from 'react-router-dom';
+
+function BottomNavbar() {
+  return (
+<>
+   
+
+    <Styledbottomnavbar>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="sticky-nav">
+  
+  <Link className="navbar-brand"to="/">Home</Link>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className='ml-auto'>
+      <Nav.Link href="#features" >Libreria</Nav.Link>
+      <Nav.Link href="#pricing">Asistencia</Nav.Link>
+      <Nav.Link href="#deets">Horario</Nav.Link>
+      <Nav.Link href="#Perfil">Perfil</Nav.Link>
+    </Nav>
+   
+  </Navbar.Collapse>
+  
+</Navbar>
+
+    </Styledbottomnavbar>
+
+
+    </>
+   
+    
+    );
+}
+export default BottomNavbar;
+
