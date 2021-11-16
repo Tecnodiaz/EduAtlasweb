d46c073c0c993730837d7b9c81fb9b58e61ad78c
daniperezdpj <daniperez.dpj@gmail.com>
2021-11-16 12:52:43 -0400

    .

---
+++ edu-atlas/src/components/Tareas.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+
+function Tareas({tarea}) {
+    return (
+      
+     
+
+          
+                  
+         
+            
+      
+          
+          
+          
+              
+          
+          <><li>{tarea.nombre}</li><li>{tarea.descripcion}</li><li>{tarea.fecha}</li></>
+          
+          
+          
+        
+      
+                  
+             
+             
+           
+           
+    
+    )
+}
+
+export default Tareas
